1063bd0e9bf0acbea3dfe3e1b1cd75111da3f681
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2026-02-18 12:08:42 +0700

    redirect both stdout stderr to dev null

--- bash/bashrc.d/ssh.sh
+++ bash/bashrc.d/ssh.sh
@@ -3,15 +3,13 @@
 PRV_KEY="$HOME/.ssh/github_ed25519"
 
 # Attempt to use existing agent
-if [ -z "$SSH_AUTH_SOCK" ] || ! ssh-add -l >/dev/null 2>&1; then
+if [ -z "$SSH_AUTH_SOCK" ] || ! ssh-add -l &>/dev/null; then
     # Start a new agent and export the variables
-    eval "$(ssh-agent -s)"
+    eval "$(ssh-agent -s)" &>/dev/null
 fi
 
 # Add key if not already added
 FINGERPRINT=$(ssh-keygen -lf "$PRV_KEY.pub" | awk '{print $2}')
-if ssh-add -L | grep -q "$FINGERPRINT"; then
-    echo "Key already added"
-else
-    ssh-add "$PRV_KEY"
+if ! ssh-add -L &>/dev/null | grep -q "$FINGERPRINT"; then
+    ssh-add "$PRV_KEY" &>/dev/null
 fi
