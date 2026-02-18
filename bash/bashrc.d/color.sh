db190fdf3ed534f6eef35a3abc1aea5bc293092a
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2026-02-19 01:03:29 +0700

    add colors

---
+++ bash/bashrc.d/color.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Reset
+RESET="\e[0m"
+
+# Background colors
+BG_BLACK="\e[40m"
+BG_RED="\e[41m"
+BG_GREEN="\e[42m"
+BG_YELLOW="\e[43m"
+BG_BLUE="\e[44m"
+BG_PURPLE="\e[45m"
+BG_CYAN="\e[46m"
+BG_WHITE="\e[47m"
+BG_PINK="\e[48;5;205m"
+BG_ORANGE="\e[48;5;208m"
+
+BG_BBLACK="\e[100m"
+BG_BRED="\e[101m"
+BG_BGREEN="\e[102m"
+BG_BYELLOW="\e[103m"
+BG_BBLUE="\e[104m"
+BG_BMAGENTA="\e[105m"
+BG_BCYAN="\e[106m"
+BG_BWHITE="\e[107m"
+
+# Foreground colors
+FG_BLACK="\e[30m"
+FG_RED="\e[31m"
+FG_GREEN="\e[32m"
+FG_YELLOW="\e[33m"
+FG_BLUE="\e[34m"
+FG_MAGENTA="\e[35m"
+FG_CYAN="\e[36m"
+FG_WHITE="\e[37m"
+FG_PINK="\e[38;5;205m"
+FG_ORANGE="\e[38;5;208m"
+
+FG_BBLACK="\e[90m"
+FG_BRED="\e[91m"
+FG_BGREEN="\e[92m"
+FG_BYELLOW="\e[93m"
+FG_BBLUE="\e[94m"
+FG_BMAGENTA="\e[95m"
+FG_BCYAN="\e[96m"
+FG_BWHITE="\e[97m"
