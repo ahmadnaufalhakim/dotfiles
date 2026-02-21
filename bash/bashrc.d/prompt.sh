58004e1b7cbfd811af26ae53ca437b166b00f27f
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2026-02-21 14:48:49 +0700

    use bzr colors

--- bash/bashrc.d/prompt.sh
+++ bash/bashrc.d/prompt.sh
@@ -23,29 +23,29 @@ build_prompt() {
 
     # Status segment
     if [[ "$exit_code" -eq 0 ]]; then
-        PS1+="${INVERT}${FG_RED}${RIGHT_SEPARATOR}${RESET}"
-        PS1+="${BOLD}${BG_RED}${FG_BLACK} ✓ "
-        PS1+="${BG_BLUE}${FG_RED}${RIGHT_SEPARATOR}"
+        PS1+="${INVERT}${FG_KT}${RIGHT_SEPARATOR}${RESET}"
+        PS1+="${BOLD}${BG_KT}${FG_BLACK} ✓ "
+        PS1+="${BG_RY}${FG_KT}${RIGHT_SEPARATOR}"
     else
         PS1+="${INVERT}${BG_DEFAULT}${FG_WHITE}${RIGHT_SEPARATOR}${RESET}"
-        PS1+="${BOLD}${BG_WHITE}${FG_RED} ✗ "
-        PS1+="${BG_BLUE}${FG_DEFAULT}${RIGHT_SEPARATOR}"
+        PS1+="${BOLD}${BG_WHITE}${FG_KT} ✗ "
+        PS1+="${BG_RY}${FG_WHITE}${RIGHT_SEPARATOR}"
     fi
 
     # User
-    PS1+="${BG_BLUE}${FG_BLACK} \u "
-    PS1+="${BG_YELLOW}${FG_BLUE}${RIGHT_SEPARATOR}"
+    PS1+="${BG_RY}${FG_BLACK} \u "
+    PS1+="${BG_NJ}${FG_RY}${RIGHT_SEPARATOR}"
 
     # Directory
-    PS1+="${BG_YELLOW}${FG_BLACK} \w "
+    PS1+="${BG_NJ}${FG_BLACK} \w "
 
     # Git branch (if in git directory)
     if [ -n "$branch" ]; then
-        PS1+="${BG_PINK}${FG_YELLOW}${RIGHT_SEPARATOR}"
-        PS1+="${BG_PINK}${FG_BLACK} ${branch_icon} ${BRANCH} ${branch} "
-        PS1+="${BG_DEFAULT}${FG_PINK}${RIGHT_SEPARATOR}"
+        PS1+="${BG_BC}${FG_NJ}${RIGHT_SEPARATOR}"
+        PS1+="${BG_BC}${FG_BLACK} ${branch_icon} ${BRANCH} ${branch} "
+        PS1+="${BG_DEFAULT}${FG_BC}${RIGHT_SEPARATOR}"
     else
-        PS1+="${BG_DEFAULT}${FG_YELLOW}${RIGHT_SEPARATOR}"
+        PS1+="${BG_DEFAULT}${FG_NJ}${RIGHT_SEPARATOR}"
     fi
 
     PS1+="${RESET}\n$ "
