82918d00ea47882354ec62b211d703e76ff759a7
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2026-03-01 14:34:34 +0700

    add `cd` aliases

--- bash/bashrc.d/core.sh
+++ bash/bashrc.d/core.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+alias home="cd ~"
+alias ..="cd .."
+alias ..2="cd ../.."
+alias ..3="cd ../../.."
+alias ..4="cd ../../../.."
+
 # Apt
 alias update="sudo apt update"
 alias upgrade="sudo apt upgrade"
