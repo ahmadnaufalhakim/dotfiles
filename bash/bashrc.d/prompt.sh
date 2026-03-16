f5816a720b7b9e3a0173e4ac76f46d2ef719600e
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2026-03-16 08:12:52 +0700

    remove echo

--- bash/bashrc.d/prompt.sh
+++ bash/bashrc.d/prompt.sh
@@ -4,7 +4,7 @@ PROMPT_DIR="${BASH_MODULE_DIR}/prompt"
 SEGMENTS_DIR="${PROMPT_DIR}/segments"
 
 for f in "${SEGMENTS_DIR}/"*.sh; do
-    [[ -r "$f" ]] && source "$f" && echo "prompt sourcing "${f}""
+    [[ -r "$f" ]] && source "$f"
 done
 
 for f in "${PROMPT_DIR}/"*.sh; do
